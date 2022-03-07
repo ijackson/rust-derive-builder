def49c58a6d8c6a3f1a78bf7c7ceb1dc07451628
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-03-07 11:26:06 +0000

    Test that builder(attrs(...)) is honoured

---
+++ derive_builder/tests/compile-fail/builder_field_attr.rs
@@ -0,0 +1,13 @@
+#[macro_use]
+extern crate derive_builder;
+
+#[derive(Debug, PartialEq, Default, Builder, Clone)]
+pub struct Lorem {
+    ok: String,
+
+    // This foolish repr() attribute generates an unused attribute warning
+    #[builder(attrs(no_such_attribute))]
+    broken: String,
+}
+
+fn main() {}
